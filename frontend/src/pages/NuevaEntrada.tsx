7ab0425355aa2f6feffdb4ddc4c7a41a8fe27bb2
yosk13 <yosk_13@msn.com>
2025-06-09 16:18:25 -0600

    Corrige URL de axios y elimina variable no usada

--- frontend/src/pages/NuevaEntrada.tsx
+++ frontend/src/pages/NuevaEntrada.tsx
@@ -26,7 +26,7 @@ export default function NuevaEntrada() {
     }
 
     try {
-      await axios.post("http://localhost:4000/entradas", {
+      await axios.post(`${import.meta.env.VITE_API_URL}/entradas`, {
         contenido,
         categoria,
       });
